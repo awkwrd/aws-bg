9d22ba36fb6b48a3fe0f18f117ebdc18e63ad897
Ilya Repnikov <Ilya_Repnikov@epam.com>
2025-11-03 16:40:38 +0400

    feat: bg v1

---
+++ variables.tf
@@ -0,0 +1,38 @@
+variable "vpc_id" {
+  description = "The ID of the pre-provisioned VPC."
+  type        = string
+}
+
+# variable "public_subnet_ids" {
+#   description = "The IDs of the pre-provisioned public subnets."
+#   type        = list(string)
+# }
+
+variable "blue_weight" {
+  description = "The traffic weight for the Blue Target Group. Specifies the percentage of traffic routed to the Blue environment."
+  type        = number
+  default     = 30
+}
+
+variable "green_weight" {
+  description = "The traffic weight for the Green Target Group. Specifies the percentage of traffic routed to the Green environment."
+  type        = number
+  default     = 70
+}
+
+variable "blue_instance_type" {
+  description = "The instance type for the Blue environment."
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "green_instance_type" {
+  description = "The instance type for the Green environment."
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "region" {
+  description = "The AWS region"
+  type        = string
+}
